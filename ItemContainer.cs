ab72cc1c00980319cfc5f99f26d3c6e22bd6ae19
Khaelus <123030177+Khaelus@users.noreply.github.com>
2023-06-04 20:55:12 +0200

    Update ItemContainer.cs

--- ItemContainer.cs
+++ ItemContainer.cs
@@ -4,38 +4,143 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-
 public class ItemContainer : MonoBehaviour
 {
     public Item itemOnContainer;
-
     public int quantity;
     public float weight;
-
     public Image itemSprite;
     public TextMeshProUGUI itemNameText;
     public TextMeshProUGUI itemWeightText;
 
+    public int equippedType; // Nuevo campo para almacenar el tipo de equipamiento (0: Sin equipar, 1: Armadura, 2: weapon)
+
 
     public void OnButtonClick()
     {
-        // Mostrar un mensaje de depuraci髇 con el ID del ItemContainer
-        Debug.Log("ItemContainer ID: " + itemOnContainer.name);
+        EquipArmor();
+        EquipWeapon();
     }
+
+   
+
+    public void EquipArmor()
+    {
+        if (itemOnContainer.selectedType == Item.Type.Armor)
+        {
+            // Si la armadura ya est谩 equipada, se desequipa
+            if (equippedType == 1)
+            {
+                equippedType = 0;
+                Player.instance.defense = 0;
+            }
+            else
+            {
+                // Desequipar cualquier item de armadura previamente equipado
+                foreach (ItemContainer itemContainer in Backpack.instance.itemContainerList)
+                {
+                    if (itemContainer.itemOnContainer.selectedType == Item.Type.Armor && itemContainer.equippedType == 1)
+                    {
+                        itemContainer.equippedType = 0;
+                        // Otra l贸gica relacionada con el desequipamiento de armadura
+                    }
+                }
+
+                // Equipar el nuevo item de armadura
+                equippedType = 1;
+                Player.instance.defense = (itemOnContainer as Armor_Item).defense;
+                // Otra l贸gica relacionada con el equipamiento de armadura
+            }
+
+            // Actualizar el texto "equipped" en el nombre del item
+            UpdateUI();
+        }
+    }
+
+    public void EquipWeapon()
+    {
+        if (itemOnContainer.selectedType == Item.Type.Weapon)
+        {
+            // Si la Weapon ya est谩 equipada, se desequipa
+            if (equippedType == 2) //2: Weapon
+            {
+                equippedType = 0;
+                Player.instance.damage = 0;
+            }
+            else
+            {
+                // Desequipar cualquier item de Weapon previamente equipado
+                foreach (ItemContainer itemContainer in Backpack.instance.itemContainerList)
+                {
+                    if (itemContainer.itemOnContainer.selectedType == Item.Type.Weapon && itemContainer.equippedType == 2)
+                    {
+                        itemContainer.equippedType = 0;
+                        // Otra l贸gica relacionada con el desequipamiento de Weapon
+                    }
+                }
+
+                // Equipar el nuevo item de Weapon
+                equippedType = 2;
+                Player.instance.damage = (itemOnContainer as Weapon_Item).damage;
+                // Otra l贸gica relacionada con el equipamiento de Weapon
+            }
+
+            // Actualizar el texto "equipped" en el nombre del item
+            UpdateUI();
+        }
+    }
+
+
+
 
     private void Update()
     {
-        weight = itemOnContainer.weight * quantity;
-        itemSprite.sprite = itemOnContainer.sprite;
-
-        if(quantity > 1)
-        {
-            itemNameText.text = $"{itemOnContainer.itemName} ({quantity})";
-        }
-        else itemNameText.text = $"{itemOnContainer.itemName}";
-
-        itemWeightText.text = weight.ToString();
+        UpdateUI();
     }
 
-}
+    public void UpdateUI()
+    {
+        weight = itemOnContainer.weight * quantity;
+        itemSprite.sprite = itemOnContainer.sprite;
+        itemWeightText.text = weight.ToString();
 
+        itemNameText.text = GetItemNameText();
+    }
+
+    private string GetItemNameText()
+    {
+        if (quantity > 1)
+        {
+            if (equippedType == 1 && itemOnContainer.selectedType == Item.Type.Armor)
+            {
+                return $"{itemOnContainer.itemName} ({quantity}) - equipped";
+            }
+            else if (equippedType == 2 && itemOnContainer.selectedType == Item.Type.Weapon)
+            {
+                return $"{itemOnContainer.itemName} ({quantity}) - equipped";
+            }
+            else
+            {
+                return $"{itemOnContainer.itemName} ({quantity})";
+            }
+        }
+        else
+        {
+            if (equippedType == 1 && itemOnContainer.selectedType == Item.Type.Armor)
+            {
+                return $"{itemOnContainer.itemName} - equipped";
+            }
+            else if (equippedType == 2 && itemOnContainer.selectedType == Item.Type.Weapon)
+            {
+                return $"{itemOnContainer.itemName} - equipped";
+            }
+            else
+            {
+                return itemOnContainer.itemName;
+            }
+        }
+    }
+
+
+
+}
